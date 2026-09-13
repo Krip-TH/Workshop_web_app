4006b779d35f90dee2371a3b90017fd4e0c5a856
Krip Topongkasem <Krip.t@ku.th>
2026-09-13 12:23:45 +0700

    [dev] add-delete-todo-endpoint

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -63,5 +63,26 @@ app.MapPut("/api/todos/{id}", (int id, TodoPutDto dto) =>
         return Results.Problem(ex.Message);
     }
 });
-app.Run();
 
+app.MapDelete("/api/todos/{id}", (int id) =>
+{
+    try
+    {
+        var todo = todos.FirstOrDefault(t => t.Id == id);
+        if (todo is null) return Results.NotFound();
+
+        todos.Remove(todo);
+        return Results.NoContent();
+    }
+    catch(ArgumentNullException ex)
+    {
+        return Results.Problem($"Parameter is null: {ex.ParamName}");
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+
+});
+
+app.Run();
